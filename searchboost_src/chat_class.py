b0ff0d17557497858f734acabcfbd4c3801aee7d
Somnerd <nikolasalexandrakis1@gmail.com>
2025-12-16 17:45:10 +0200

    intergrate api_call into chat_class

--- searchboost_src/chat_class.py
+++ searchboost_src/chat_class.py
@@ -1,5 +1,10 @@
+import asyncio
+import openai
+from openai import AsyncOpenAI
+
+
 class ChatDetails:
-    def __init__(self, model, prompt,role="user"):
+    def __init__(self, model: str, prompt: str,role:str ="user"):
         self.model = model
         self.prompt = prompt
         self.role = role
@@ -8,4 +13,23 @@ class ChatDetails:
             "web_search": True
         }
 
+    async def api_call(self, API_KEY:str):
+        try:
+            client = openai.OpenAI(
+                api_key = API_KEY,  # or os.getenv("POE_API_KEY")
+                base_url = "https://api.poe.com/v1",
+            )
 
+            chat = await client.chat.completions.create(
+                model = self.model,
+                messages = [{"role": self.role, "content": self.prompt}],
+                extra_body = self.extra_body
+            )
+
+            if chat.choices and chat.choices[0].message.content:
+                print(chat.choices[0].message.content)
+                return chat.choices[0].message.content
+
+        except Exception as e:
+            print(f"Error querying Poe API: {e}")
+            return "Error: Unable to connect to the LLM."
